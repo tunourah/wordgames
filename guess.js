3aa369d7a674a5dee5cd09abeaaf91ab767199dc
noura <noura.tu19@gmail.com>
2024-04-16 11:14:42 +0300

    frist

---
+++ guess.js
@@ -0,0 +1,5 @@
+//setting  game name 
+let gameName = "Guess the word!";
+document.title = gameName;
+document.querySelector('h1').innerHTML = gameName;
+document.querySelector('footer').innerHTML = `${gameName} &copy; 2024`;
